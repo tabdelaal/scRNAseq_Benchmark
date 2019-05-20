1ba0acb25cbce7772f92d8c19a7e2664067744c7
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2019-05-20 18:54:02 +0200

    Update scPred

--- Scripts/run_scPred.R
+++ Scripts/run_scPred.R
@@ -1,11 +1,28 @@
-Run_scPred<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NULL, num_of_genes = NULL){
+run_scPred<-function(DataPath,LabelsPath,CV_RDataPath,OutputDir,GeneOrderPath = NULL,NumGenes = NULL){
+  "
+  run scPred
+  Wrapper script to run scPred on a benchmark dataset with 5-fold cross validation,
+  outputs lists of true and predicted cell labels as csv files, as well as computation time.
+  
+  Parameters
+  ----------
+  DataPath : Data file path (.csv), cells-genes matrix with cell unique barcodes 
+  as row names and gene names as column names.
+  LabelsPath : Cell population annotations file path (.csv).
+  CV_RDataPath : Cross validation RData file path (.RData), obtained from Cross_Validation.R function.
+  OutputDir : Output directory defining the path of the exported file.
+  GeneOrderPath : Gene order file path (.csv) obtained from feature selection, 
+  defining the genes order for each cross validation fold, default is NULL.
+  NumGenes : Number of genes used in case of feature selection (integer), default is NULL.
+  "
+  
   Data <- read.csv(DataPath,row.names = 1)
   Labels <- as.matrix(read.csv(LabelsPath))
   load(CV_RDataPath)
   Labels <- as.vector(Labels[,col_Index])
   Data <- Data[Cells_to_Keep,]
   Labels <- Labels[Cells_to_Keep]
-  if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
+  if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
     GenesOrder = read.csv(GeneOrderPath)
   }
   
@@ -22,14 +39,14 @@ Run_scPred<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NULL, num
   Data = t(as.matrix(Data))
   
   for (i in c(1:n_folds)){
-    if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
-      sce <- SingleCellExperiment(list(normcounts = Data[as.vector(GenesOrder[c(1:num_of_genes),i])+1,Train_Idx[[i]]]), 
+    if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
+      sce <- SingleCellExperiment(list(normcounts = Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Train_Idx[[i]]]), 
                                   colData = data.frame(cell_type1 = Labels[Train_Idx[[i]]]))
       sce_counts <- normcounts(sce)
       sce_cpm <- apply(sce_counts, 2, function(x) (x/sum(x))*1000000)
       sce_metadata <- as.data.frame(colData(sce))
       
-      sce_test <- SingleCellExperiment(list(normcounts = Data[as.vector(GenesOrder[c(1:num_of_genes),i])+1,Test_Idx[[i]]]), 
+      sce_test <- SingleCellExperiment(list(normcounts = Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Test_Idx[[i]]]), 
                                        colData = data.frame(cell_type1 = Labels[Test_Idx[[i]]]))
       sce_counts_test <- normcounts(sce_test)
       sce_cpm_test <- apply(sce_counts_test, 2, function(x) (x/sum(x))*1000000)
@@ -75,16 +92,19 @@ Run_scPred<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NULL, num
   Pred_Labels_scPred <- as.vector(unlist(Pred_Labels_scPred))
   Training_Time_scPred <- as.vector(unlist(Training_Time_scPred))
   Testing_Time_scPred <- as.vector(unlist(Testing_Time_scPred))
-  if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
-    write.csv(True_Labels_scPred,paste('True_Labels_scPred_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Pred_Labels_scPred,paste('Pred_Labels_scPred_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Training_Time_scPred,paste('Training_Time_scPred_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Testing_Time_scPred,paste('Testing_Time_scPred_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
+  
+  setwd(OutputDir)
+  
+  if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
+    write.csv(True_Labels_scPred,paste('scPred_',NumGenes,'_True_Labels.csv', sep = ''),row.names = FALSE)
+    write.csv(Pred_Labels_scPred,paste('scPred_',NumGenes,'_Pred_Labels.csv', sep = ''),row.names = FALSE)
+    write.csv(Training_Time_scPred,paste('scPred_',NumGenes,'_Training_Time.csv', sep = ''),row.names = FALSE)
+    write.csv(Testing_Time_scPred,paste('scPred_',NumGenes,'_Testing_Time.csv', sep = ''),row.names = FALSE)
   }
   else{
-    write.csv(True_Labels_scPred,'True_Labels_scPred.csv',row.names = FALSE)
-    write.csv(Pred_Labels_scPred,'Pred_Labels_scPred.csv',row.names = FALSE)
-    write.csv(Training_Time_scPred,'Training_Time_scPred.csv',row.names = FALSE)
-    write.csv(Testing_Time_scPred,'Testing_Time_scPred.csv',row.names = FALSE)
+    write.csv(True_Labels_scPred,'scPred_True_Labels.csv',row.names = FALSE)
+    write.csv(Pred_Labels_scPred,'scPred_Pred_Labels.csv',row.names = FALSE)
+    write.csv(Training_Time_scPred,'scPred_Training_Time.csv',row.names = FALSE)
+    write.csv(Testing_Time_scPred,'scPred_Testing_Time.csv',row.names = FALSE)
   }
 }
