3845074f3270164ee52e3ed4ffc39377c0a50163
tabdelaal <31687847+tabdelaal@users.noreply.github.com>
2019-05-20 18:55:05 +0200

    Update scNet

--- Scripts/run_singleCellNet.R
+++ Scripts/run_singleCellNet.R
@@ -1,4 +1,21 @@
-Run_singleCellNet<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NULL, num_of_genes = NULL){
+run_singleCellNet<-function(DataPath,LabelsPath,CV_RDataPath,OutputDir,GeneOrderPath = NULL,NumGenes = NULL){
+  "
+  run singleCellNet
+  Wrapper script to run singleCellNet on a benchmark dataset with 5-fold cross validation,
+  outputs lists of true and predicted cell labels as csv files, as well as computation time.
+  
+  Parameters
+  ----------
+  DataPath : Data file path (.csv), cells-genes matrix with cell unique barcodes 
+  as row names and gene names as column names.
+  LabelsPath : Cell population annotations file path (.csv).
+  CV_RDataPath : Cross validation RData file path (.RData), obtained from Cross_Validation.R function.
+  OutputDir : Output directory defining the path of the exported file.
+  GeneOrderPath : Gene order file path (.csv) obtained from feature selection, 
+  defining the genes order for each cross validation fold, default is NULL.
+  NumGenes : Number of genes used in case of feature selection (integer), default is NULL.
+  "
+  
   Data <- read.csv(DataPath,row.names = 1)
   colnames(Data) <- gsub('_','.',colnames(Data), fixed = TRUE)
   Labels <- as.matrix(read.csv(LabelsPath))
@@ -6,7 +23,7 @@ Run_singleCellNet<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NU
   Labels <- as.vector(Labels[,col_Index])
   Data <- Data[Cells_to_Keep,]
   Labels <- Labels[Cells_to_Keep]
-  if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
+  if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
     GenesOrder = read.csv(GeneOrderPath)
   }
   
@@ -22,9 +39,9 @@ Run_singleCellNet<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NU
   Data = t(as.matrix(Data))              # deals also with sparse matrix
   
   for(i in c(1:n_folds)){
-    if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
-      DataTrain <- Data[as.vector(GenesOrder[c(1:num_of_genes),i])+1,Train_Idx[[i]]]
-      DataTest <- Data[as.vector(GenesOrder[c(1:num_of_genes),i])+1,Test_Idx[[i]]]
+    if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
+      DataTrain <- Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Train_Idx[[i]]]
+      DataTest <- Data[as.vector(GenesOrder[c(1:NumGenes),i])+1,Test_Idx[[i]]]
     }
     else{
       DataTrain <- Data[,Train_Idx[[i]]]
@@ -55,16 +72,19 @@ Run_singleCellNet<-function(DataPath,LabelsPath,CV_RDataPath, GeneOrderPath = NU
   Pred_Labels_singleCellNet <- as.vector(unlist(Pred_Labels_singleCellNet))
   Training_Time_singleCellNet <- as.vector(unlist(Training_Time_singleCellNet))
   Testing_Time_singleCellNet <- as.vector(unlist(Testing_Time_singleCellNet))
-  if(!is.null(GeneOrderPath) & !is.null (num_of_genes)){
-    write.csv(True_Labels_singleCellNet,paste('True_Labels_singleCellNet_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Pred_Labels_singleCellNet,paste('Pred_Labels_singleCellNet_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Training_Time_singleCellNet,paste('Training_Time_singleCellNet_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
-    write.csv(Testing_Time_singleCellNet,paste('Testing_Time_singleCellNet_',num_of_genes,'.csv', sep = ''),row.names = FALSE)
+  
+  setwd(OutputDir)
+  
+  if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
+    write.csv(True_Labels_singleCellNet,paste('singleCellNet_',NumGenes,'_True_Labels.csv', sep = ''),row.names = FALSE)
+    write.csv(Pred_Labels_singleCellNet,paste('singleCellNet_',NumGenes,'_Pred_Labels.csv', sep = ''),row.names = FALSE)
+    write.csv(Training_Time_singleCellNet,paste('singleCellNet_',NumGenes,'_Training_Time.csv', sep = ''),row.names = FALSE)
+    write.csv(Testing_Time_singleCellNet,paste('singleCellNet_',NumGenes,'_Testing_Time.csv', sep = ''),row.names = FALSE)
   }
   else{
-    write.csv(True_Labels_singleCellNet,'True_Labels_singleCellNet.csv',row.names = FALSE)
-    write.csv(Pred_Labels_singleCellNet,'Pred_Labels_singleCellNet.csv',row.names = FALSE)
-    write.csv(Training_Time_singleCellNet,'Training_Time_singleCellNet.csv',row.names = FALSE)
-    write.csv(Testing_Time_singleCellNet,'Testing_Time_singleCellNet.csv',row.names = FALSE)
+    write.csv(True_Labels_singleCellNet,'singleCellNet_True_Labels.csv',row.names = FALSE)
+    write.csv(Pred_Labels_singleCellNet,'singleCellNet_Pred_Labels.csv',row.names = FALSE)
+    write.csv(Training_Time_singleCellNet,'singleCellNet_Training_Time.csv',row.names = FALSE)
+    write.csv(Testing_Time_singleCellNet,'singleCellNet_Testing_Time.csv',row.names = FALSE)
   }
 }
