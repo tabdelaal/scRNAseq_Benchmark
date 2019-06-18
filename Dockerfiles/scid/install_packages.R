bfa68e112cf31fcb326e2615e71c89bcb5031b3a
ddhoogduin <ddhoogduin@gmail.com>
2019-06-18 11:38:20 +0200

    default biomanager install

--- Dockerfiles/scid/install_packages.R
+++ Dockerfiles/scid/install_packages.R
@@ -1,6 +1,6 @@
 withCallingHandlers({
   install.packages("BiocManager", repos="https://cloud.r-project.org/")
-  BiocManager::install(version = "devel", ask = FALSE);
+  BiocManager::install(ask = FALSE);
   BiocManager::install(c("scater", "MAST"))
   install.packages("devtools", repos="https://cloud.r-project.org/")
   devtools::install_github("satijalab/seurat")
--- Dockerfiles/scmap/install_packages.R
+++ Dockerfiles/scmap/install_packages.R
@@ -1,6 +1,6 @@
 withCallingHandlers({
   install.packages("BiocManager", repos="https://cloud.r-project.org/")
-  BiocManager::install(version = "devel", ask = FALSE)
+  BiocManager::install(ask = FALSE)
   BiocManager::install("SingleCellExperiment")
   install.packages("devtools", repos="https://cloud.r-project.org/")
   devtools::install_github("hemberg-lab/scmap")
