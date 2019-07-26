0c94df128f09f54f749b4900284198c2c39c0cb5
lcmmichielsen <42767830+lcmmichielsen@users.noreply.github.com>
2019-07-26 14:41:28 +0200

    version2

--- evaluate.R
+++ evaluate.R
@@ -1,15 +1,16 @@
-evaluate <- function(TrueLabelsPath, PredLabelsPath){
+evaluate <- function(TrueLabelsPath, PredLabelsPath, Indices = NULL){
   "
   Script to evaluate the performance of the classifier.
   It returns multiple evaluation measures: the confusion matrix, median F1-score, F1-score for each class, accuracy, percentage of unlabeled, population size. 
-
-  The percentage of unlabeled cells is find by checking for cells that are labeled 'Unassigned', 'unassigned', 'Unknown', 'Nodexx', 'rand', or 'ambiguous'.
+  
+  The percentage of unlabeled cells is find by checking for cells that are labeled 'Unassigned', 'unassigned', 'Unknown', 'unknown', 'Nodexx', 'rand', or 'ambiguous'.
   
   Parameters
   ----------
   TrueLabelsPath: csv file with the true labels (format: one column, no index)
   PredLabelsPath: csv file with the predicted labels (format: one column, no index)
-
+  Indices: which part of the csv file should be read (e.g. if more datasets are tested at the same time) (format: c(begin, end))
+  
   Returns
   -------
   Conf: confusion matrix
@@ -23,6 +24,11 @@ evaluate <- function(TrueLabelsPath, PredLabelsPath){
   true_lab <- unlist(read.csv(TrueLabelsPath))
   pred_lab <- unlist(read.csv(PredLabelsPath))
   
+  if (! is.null(Indices)){
+    true_lab <- true_lab[Indices]
+    pred_lab <- pred_lab[Indices]
+  }
+  
   unique_true <- unlist(unique(true_lab))
   unique_pred <- unlist(unique(pred_lab))
   
@@ -32,8 +38,8 @@ evaluate <- function(TrueLabelsPath, PredLabelsPath){
   
   pred_lab = gsub('Node..','Node',pred_lab)
   
-  conf_F1 <- table(true_lab,pred_lab,exclude = c('unassigned','Unassigned','Unknown','rand','Node','ambiguous'))
-  
+  conf_F1 <- table(true_lab,pred_lab,exclude = c('unassigned','Unassigned','Unknown','rand','Node','ambiguous','unknown'))
+
   F1 <- vector()
   sum_acc <- 0
   
@@ -53,12 +59,14 @@ evaluate <- function(TrueLabelsPath, PredLabelsPath){
     }
   }
   
+  pop_size <- pop_size[pop_size > 0]
+  
   names(F1) <- names(pop_size)
   
   med_F1 <- median(F1)
-
+  
   total <- length(pred_lab)
-  num_unlab <- sum(pred_lab == 'unassigned') + sum(pred_lab == 'Unassigned') + sum(pred_lab == 'rand') + sum(pred_lab == 'Unknown') + sum(pred_lab == 'Node') + sum(pred_lab == 'ambiguous')
+  num_unlab <- sum(pred_lab == 'unassigned') + sum(pred_lab == 'Unassigned') + sum(pred_lab == 'rand') + sum(pred_lab == 'Unknown') + sum(pred_lab == 'unknown') + sum(pred_lab == 'Node') + sum(pred_lab == 'ambiguous')
   per_unlab <- num_unlab / total
   
   acc <- sum_acc/sum(conf_F1)
